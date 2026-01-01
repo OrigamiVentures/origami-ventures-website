c3a6f0433461f5c7951383b5ec6274c1242e09ac
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2026-01-01 13:23:06 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./styles/Landing.css";
+
+function App() {
+  return (
+    <main>
+      <div className="gradient-background">
+        <section>
+          <div className="container-main">
+            <h1 className="hero-heading-h1">
+              Building bold brands with <span>thoughtful design</span>
+            </h1>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
